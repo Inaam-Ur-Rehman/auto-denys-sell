afb4030c2ad4d500d14986c95a2c5d4f9f529cd0
Muhammad Inam Ur Rehman <inaamurrehman343@gmail.com>
2024-01-04 19:10:56 +0500

    Added: all filessss

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,17 +1,15 @@
 <div class="relative items-center w-full h-full px-4 text-white !bg-center !bg-no-repeat !bg-cover md:px-24 drop-shadow-lg"
-    style="background-image: url('{{ asset('images/hero.png') }}')
->
-    <div class="grid grid-cols-1 py-8 mx-auto
-    md:grid-cols-2 max-w-screen-theme-screen">
-    <div class="z-40 flex flex-col gap-4 pt-32 pb-8 md:pb-24 text-center md:text-left max-w-[600px]">
-        <h1 class="text-2xl font-bold md:text-4xl">Verkoop uw auto, bestelwagen of mobilhome zorgeloos aan de beste
-            prijs!</h1>
-        <p class="text-lg md:text-xl max-w-[80%] font-light pb-4 mx-auto md:mx-0">
-            Vul onderstaand formulier in zodat we een correct beeld hebben over uw voertuig. Dit duurt slecht 30
-            seconden.
-            We nemen doorgaans dezelfde dag contact op.
-        </p>
+    style="background-image: url('{{ asset('images/hero.png') }}')">
+    <div class="grid grid-cols-1 py-8 mx-auto md:grid-cols-2 max-w-screen-theme-screen">
+        <div class="z-40 flex flex-col gap-4 pt-32 pb-8 md:pb-24 text-center md:text-left max-w-[600px]">
+            <h1 class="text-2xl font-bold md:text-4xl">Verkoop uw auto, bestelwagen of mobilhome zorgeloos aan de beste
+                prijs!</h1>
+            <p class="text-lg md:text-xl max-w-[80%] font-light pb-4 mx-auto md:mx-0">
+                Vul onderstaand formulier in zodat we een correct beeld hebben over uw voertuig. Dit duurt slecht 30
+                seconden.
+                We nemen doorgaans dezelfde dag contact op.
+            </p>
+        </div>
+        <div></div>
     </div>
-    <div></div>
-</div>
 </div>
